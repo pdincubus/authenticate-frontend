5c3d33bcaea50d035ef989a29c218307fc11746f
Phil Steer <phil.steer@engineering.digital.dwp.gov.uk>
2019-10-29 09:43:55 +0000

    First pass at views from Powerpoint

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
-// Add your routes here - above the module.exports line
+router.post('/v1/password-reset-check-code', function (req, res) {
+    let userCode = req.session.data['password-reset-code'];
+
+    if (parseInt(userCode) === 123456) {
+        res.redirect('/v1/password-reset-new-password');
+    } else {
+        res.redirect('/v1/password-reset-invalid-code');
+    }
+});
 
 module.exports = router
